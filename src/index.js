fbd3a29964b44fbc451fb919eafd91645adb4e42
Utsargo Roy <royutsargo@gmail.com>
2023-01-26 14:52:53 +0600

    Add changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 // import ReactDOM from "react-dom"
 import ReactDOM from 'react-dom/client';
-import TodoContainer from './components/TodoContainer';
+import TodoContainer from './functionBased/components/TodoContainer';
+import './functionBased/App.scss';
 
 // ReactDOM.render(<TodoContainer />, document.getElementById("root"))
 const root = ReactDOM.createRoot(document.getElementById('root'));
