049612592c5e7f8c6843c9f50e82f73f89404805
Utsargo Roy <royutsargo@gmail.com>
2023-01-27 00:38:20 +0600

    Complete app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-// import ReactDOM from "react-dom"
-import ReactDOM from 'react-dom/client';
-import TodoContainer from './functionBased/components/TodoContainer';
-import './functionBased/App.scss';
-
-// ReactDOM.render(<TodoContainer />, document.getElementById("root"))
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import TodoContainer from './components/TodoContainer';
+import './App.scss';
+// eslint-disable-next-line
+ReactDOM.render(
   <React.StrictMode>
-    <TodoContainer />
+    <Router>
+      <TodoContainer />
+    </Router>
   </React.StrictMode>,
+  document.getElementById('root'),
 );
